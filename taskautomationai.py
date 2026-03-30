e5ab42a6363879cbf79d267a2cfd0697a99a83fe
Nilesh Saraswat <saraswatnilesh3@gmail.com>
2026-03-30 22:13:36 +0530

    Refactor task automation AI for improved functionality

--- taskautomationai.py
+++ taskautomationai.py
@@ -3,13 +3,10 @@ import datetime
 import speech_recognition as sr
 import wikipedia
 import webbrowser
-import os
-import smtplib
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-#print(voices) to know no. of voices available in system
-engine.setProperty('voice',voices[2].id)
+engine.setProperty('voice', voices[0].id) 
 
 def speak(audio):
     engine.say(audio)
@@ -17,53 +14,51 @@ def speak(audio):
 
 def wishme():
     hour = int(datetime.datetime.now().hour)
-    if hour >=0 and hour <12:
-        speak("good morning")
-    elif hour>=12 and hour<18:
-        speak("good afternoon")
+    if 0 <= hour < 12:
+        speak("Good morning")
+    elif 12 <= hour < 18:
+        speak("Good afternoon")
     else:
-        speak("good evening")
-    speak("i am jarvis sir .please tell me how i can help you")
+        speak("Good evening")
+    speak("I am Jarvis sir. Please tell me how I can help you.")
 
 def takecommand():
-    '''takes input from the user and returns string output'''
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print("lisentening.....")
-        r.pause_threshold = 1#time after which if you don't speak then code with excute by default it's 
+        print("Listening...")
+        r.adjust_for_ambient_noise(source, duration=1)
         audio = r.listen(source)
 
     try:
-        print("recongnizing...")
+        print("Recognizing...")
         query = r.recognize_google(audio, language='en-IN')
-        print(f"user said :{query}\n")
-
+        print(f"User said: {query}\n")
     except Exception as e:
-        print(e)
-        print("say that again please")
+        print("Say that again please...")
         return "None"
-
     return query
-    
+
 if __name__ == '__main__':
     wishme()
-    while true: #while true for infinite time
+    while True:
         query = takecommand().lower()
         if 'wikipedia' in query:
-            speak('searching wikipedia.....')
-            query = query.replace("wikipedia","")
-            results = wikipedia.summary(query,sentences=2)
-            speak("according to wikipedia")
-            print(results)
-            speak(results)
-
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            try:
+                results = wikipedia.summary(query, sentences=2)
+                speak("According to Wikipedia")
+                print(results)
+                speak(results)
+            except Exception:
+                speak("Sorry, I couldn't find anything.")
         elif 'open youtube' in query:
-            webbrowser.open("youtube.com")
+            webbrowser.open("https://youtube.com")
         elif 'open google' in query:
-            webbrowser.open("google.com")
+            webbrowser.open("https://google.com")
         elif 'the time' in query:
-            strtime = datetime.datetime.now().strftime(("%H:%M:%S"))
-            speak(f"the time is :{strtime}")
+            strtime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"The time is {strtime}")
             
 
 
